c1d198e218eaaef946e672919fb70e58a05132fc
olena099 <142909583+olena099@users.noreply.github.com>
2024-01-09 06:55:04 -0800

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,14 @@
+
+    for item in data:
+import random
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+    return data
+
+def generate_random_data():
+def main():
+
+
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
